c0b60948b37c11b71c2f52b64c03e8dec3adbce7
Welljones <wjmhate@gmail.com>
2024-02-27 23:27:00 +0800

    Adding JS, showind social network

---
+++ main.js
@@ -0,0 +1,6 @@
+// Show Social Networks
+const showSocial = (toggleCard, socialCard) =>{
+    const toggle = document.getElementById(toggleCard),
+          social = document.getElementById(socialCard)
+}
+showSocial('card-toggle', 'card-social')
