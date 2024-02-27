365983030a52fdbd6411a8b23b40146b9ea16d3c
Welljones <wjmhate@gmail.com>
2024-02-27 23:30:59 +0800

    Adding animations with js & css

--- main.js
+++ main.js
@@ -2,5 +2,10 @@
 const showSocial = (toggleCard, socialCard) =>{
     const toggle = document.getElementById(toggleCard),
           social = document.getElementById(socialCard)
+
+    toggle.addEventListener('click', () =>){
+        // We add the animation class to the div tag with the card__social class
+        social.classList.toggle('animation')
+    }
 }
 showSocial('card-toggle', 'card-social')
